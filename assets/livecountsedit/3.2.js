11f65e02080b6fc8c9f41e4e59a02f8251de34cb
lcedit <69283105+lcedit@users.noreply.github.com>
2020-08-16 10:23:49 -0700

    fix 2

--- assets/livecountsedit/3.2.js
+++ assets/livecountsedit/3.2.js
@@ -532,12 +532,12 @@ maxValues = 500;
 var r = ![];
 /** @type {boolean} */
 noDecimals = !![];
-const canvas = document["getElementById"](_0x2fed("0x24"));
+var canvas = document["getElementById"](_0x2fed("0x24"));
 if (canvas[_0x2fed("0x3")]) {
   ctx = canvas[_0x2fed("0x3")]("2d");
 }
-const width = canvas[_0x2fed("0xb")];
-const height = canvas[_0x2fed("0xd")];
+var width = canvas[_0x2fed("0xb")];
+var height = canvas[_0x2fed("0xd")];
 /**
  * @param {?} isReturnSeconds
  * @param {boolean} isShowHours
